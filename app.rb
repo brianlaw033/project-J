60d9a46b6114ebe089aada6242d3f53359245d27
Richard-NG <richard.chihong.ng@gmail.com>
2017-04-20 12:20:29 +0800

    profile update done

--- app.rb
+++ app.rb
@@ -32,6 +32,26 @@ get('/profile') do
   erb(:profile)
 end
 
+get('/profile/edit') do
+  @profile_edit = true
+  @user = User.find(session[:user_id])
+  erb(:profile)
+end
+
+patch ('/profile') do
+  user = User.find(session[:user_id])
+  username = params.fetch('username')
+  name = params.fetch('name')
+  gender = params.fetch('gender')
+  image_url = params.fetch('image_url')
+  dob = params.fetch('dob')
+  user.update({:username => username, :name => name, :gender => gender, :image_url => image_url, :dob => dob})
+  session.clear
+  session[:user_id] = user.id
+  session[:username] = user.username
+  redirect to ('/profile')
+end
+
 post("/user") do
   username = params.fetch('username')
   user = User.find_by username: username
